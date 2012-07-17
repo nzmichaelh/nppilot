c2845e9da05f110cf8261dbf26a7ba90b51125e7
Michael Hope <michaelh@juju.net.nz>
2012-07-17 22:23:25 +1200

    Added a Doxygen file and some basic comments.

--- lib/timer.h
+++ lib/timer.h
@@ -1,7 +1,20 @@
+/*
+ * Periodic or single shot timer.
+ */
+
 #pragma once
 
 #include <cstdint>
 
+/**
+ * Periodic or single shot timer.
+ *
+ * Periodic timers start on program start and fire at a fixed
+ * interval.  Single shot timers have a reload of `Stopped` and are
+ * started by calling start(remain).
+ *
+ * Timers can trigger a thread.  Use -1 to create a detached timer.
+ */
 struct Timer
 {
 public:
@@ -15,16 +28,22 @@ public:
 
     void stop() { _remain = Stopped; }
     void start(int remain) { _remain = remain; }
-    void tick(const Fixed& fixed);
 
     static void tick_all();
 
 private:
     static const uint16_t Reserved = 0xFFFE;
 
+    void tick(const Fixed& fixed);
+
     uint16_t _remain;
 };
 
+/**
+ * Make a timer called `timer_name` that triggers the thread `_id` and
+ * has the period `_period`.  See Switcher::None and
+ * Timer::Stopped for detached and single shot timers.
+ */
 #define MAKE_TIMER(_name, _id, _period)          \
     Timer timer_##_name __attribute__((section(".timers")));     \
     Timer::Fixed timer_fixed_##_name                      \
